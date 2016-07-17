95102eaeeaf841fc0077a6b58b4f585fbb46bfac
JY01095902 <JY01095902@126.COM>
2016-07-17 22:29:05 +0800

    显示/隐藏工具栏测试

--- app/js/my-app.js
+++ app/js/my-app.js
@@ -1,9 +1,22 @@
 // Initialize your app
-var myApp = new Framework7();
+var myApp = new Framework7({
+    pushState: true,
+    pushStateSeparator: "#"
+});
 
 // Export selectors engine
 var $$ = Dom7;
 
+var mainView = myApp.addView('.view-main', {
+    // Because we use fixed-through navbar we can enable dynamic navbar
+    dynamicNavbar: true
+});
+
+
+var indexView = myApp.addView('.indexView', {
+    // Because we use fixed-through navbar we can enable dynamic navbar
+    dynamicNavbar: true
+});
 // Add views
 var accountsView = myApp.addView('#accountsView', {
     // Because we use fixed-through navbar we can enable dynamic navbar
@@ -16,4 +29,33 @@ var billsView = myApp.addView('#billsView', {
 var meView = myApp.addView('#meView', {
     // Because we use fixed-through navbar we can enable dynamic navbar
     dynamicNavbar: true
+});
+
+/*$$(document).on('pageInit', function (e) {
+    var page = e.detail.page;
+    // Code for About page
+    if (page.name === 'about') {
+        // We need to get count GET parameter from URL (about.html?count=10)
+        var count = page.query.count;
+        // Now we can generate some dummy list
+        var listHTML = '<ul>';
+        for (var i = 0; i < count; i++) {
+            listHTML += '<li>' + i + '</li>';
+        }
+        listHTML += '</ul>';
+        // And insert generated list to page content
+        $$(page.container).find('.page-content').append(listHTML);
+    }
+    // Code for Services page
+    if (page.name === 'services') {
+        myApp.alert('Here comes our services!');
+    }
+});*/
+
+myApp.onPageBack('settingsPage', function (page) {
+    myApp.showToolbar('.toolbar');
+});
+
+myApp.onPageInit('settingsPage', function (page) {
+    myApp.hideToolbar('.toolbar');
 });
--- dist/js/my-app.js
+++ dist/js/my-app.js
@@ -1,8 +1,16 @@
 // Initialize your app
-var myApp = new Framework7();
+var myApp = new Framework7({
+    pushState: true,
+    pushStateSeparator: "#"
+});
 
 // Export selectors engine
 var $$ = Dom7;
+
+var mainView = myApp.addView('.view-main', {
+    // Because we use fixed-through navbar we can enable dynamic navbar
+    dynamicNavbar: true
+});
 
 // Add views
 var accountsView = myApp.addView('#accountsView', {
@@ -16,4 +24,16 @@ var billsView = myApp.addView('#billsView', {
 var meView = myApp.addView('#meView', {
     // Because we use fixed-through navbar we can enable dynamic navbar
     dynamicNavbar: true
+});
+
+myApp.onPageReinit('mePage', function (page) {
+    myApp.showToolbar('.toolbar');
+});
+
+myApp.onPageInit('settingsPage', function (page) {
+    myApp.hideToolbar('.toolbar');
+});
+
+myApp.onPageInit('aboutPage', function (page) {
+    myApp.hideToolbar('.toolbar');
 });
