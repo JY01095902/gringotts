5c48a791d2f23d7dfdcb74ad891c718074bc0c70
shi.yanxun <shi.yanxun.co.kr>
2016-07-21 14:37:18 +0800

    dist

---
+++ dist/components/chooseAccount.js
@@ -0,0 +1,27 @@
+ChooseAccount = React.createClass({
+    render: function () {
+        var vaults = [];
+        for(var i in this.props.vaults){
+            var vault = this.props.vaults[i];
+            vaults.push(
+                            <ChooseAccountVault key={vault.id}
+                                id={vault.id} 
+                                name={vault.name} 
+                                chests={vault.chests}
+                                fullName={this.props.name}
+                                onChecked={this.props.onChecked}
+                                />
+                        );
+        }
+        return (
+            <div>
+                <div className="content-block-title">{this.props.name}</div>
+                <div className="list-block accordion-list">
+                    <ul>
+                        {vaults}
+                    </ul>
+                </div>
+            </div>
+        );
+    }
+});
